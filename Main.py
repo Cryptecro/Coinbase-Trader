ad44ed6b1419407ffcd5371468563de030d7b6ed
Zach <thesonyman101@gmail.com>
2016-05-12 18:31:24 -0700

    Added API login

--- Main.py
+++ Main.py
@@ -1,3 +1,32 @@
-import sys 
-import money as Money 
-import time 
+ import json, hmac, hashlib, time, requests
+ import money 
+ from requests.auth import AuthBase #Do pip install requests
+ 
+ API_KEY = 'API_KEY'
+ API_SECRET = 'API_KEY'
+ 
+ # Create custom authentication for Coinbase API
+class CoinbaseWalletAuth(AuthBase):
+    def __init__(self, api_key, secret_key):
+        self.api_key = api_key
+        self.secret_key = secret_key
+
+    def __call__(self, request):
+        timestamp = str(int(time.time()))
+        message = timestamp + request.method + request.path_url + (request.body or '')
+        signature = hmac.new(self.secret_key, message, hashlib.sha256).hexdigest()
+
+        request.headers.update({
+            'CB-ACCESS-SIGN': signature,
+            'CB-ACCESS-TIMESTAMP': timestamp,
+            'CB-ACCESS-KEY': self.api_key,
+        })
+        return request
+
+api_url = 'https://api.coinbase.com/v2/'
+auth = CoinbaseWalletAuth(API_KEY, API_SECRET)
+
+# Get current user
+r = requests.get(api_url + 'user', auth=auth)
+print r.json()
+# {u'data': {u'username': None, u'resource': u'user', u'name': u'User'...
